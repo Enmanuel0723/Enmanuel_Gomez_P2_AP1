4560920172209099e481f10ddc3936c16337ff87
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-03-10 18:59:45 -0400

    Se creo el servicio y el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,22 @@
-﻿namespace Enmanuel_Gomez_P2_AP1.DAL
+﻿using Enmanuel_gomez_P2_AP1.Models
+using Microsoft.EntityFrameworkCore;
+
+
+public class Context(DbContextOptions<Context> options) : DbContext(options)
 {
-    public class Contexto
+    public DbSet<Modelos> Modelos { get; set; }
+    public DbSet<ModelosDetalle> ModelosDetalle { get; set; }
+    public DbSet<Articulos> Articulos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
+        {
+            new Articulos() {ArticuloId = 1, Nombre = "Campo1", Existencia = 50, Costo = 15, Precio = 30},
+            new Articulos() {ArticuloId = 2, Nombre = "Campo2", Existencia = 40, Costo = 25, Precio = 60},
+            new Articulos() {ArticuloId = 3, Nombre = "Campo3", Existencia = 30, Costo = 35, Precio = 70},
+        });
     }
 }
---
+++ Services/CiudadesServices.cs
@@ -0,0 +1,19 @@
+﻿using Enmanuel_Gomez_P2_AP1.DAL;
+using Enmanuel_Gomez_P2_AP1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Enmanuel_Gomez_P2_AP1.Services;
+
+public class ArticulosService(IDbContextFactory<Context> DbFactory)
+{
+    public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        return await contexto.Articulos
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
